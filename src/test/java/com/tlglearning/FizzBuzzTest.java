fed3cf027c88aed2f79060e93f8350208ef7113d
nick-bennett <nick@nickbenn.com>
2022-09-20 13:36:49 -0600

    Added test methods for the different possible outcomes.

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,29 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
-  void valueOf() {
+  void valueOf_fizz() {
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
   }
+
+  @Test
+  void valueOf_buzz() {
+    assertEquals(Set.of(FizzBuzz.BUZZ), FizzBuzz.valueOf(10));
+  }
+
+  @Test
+  void valueOf_fizzBuzz() {
+    assertEquals(Set.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), FizzBuzz.valueOf(15));
+  }
+
+  @Test
+  void valueOf_none() {
+    assertEquals(Set.of(), FizzBuzz.valueOf(11));
+  }
+
 }
